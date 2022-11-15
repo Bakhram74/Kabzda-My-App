582fec496e682a50f54c07874a837cc00c6b691a
Bakhram <abdurakhimov93@list.ru>
2022-11-15 07:16:31 +0300

    React.memo 17

---
+++ my-app/src/components/React_memo/React_Memo.stories.tsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+export default {
+    title: "React memo"
+}
+
+
+const NewMessagesCounter = (props:{counter:number})=>{
+    return(
+        <div>{props.counter}</div>
+    )
+}
+const UsersSecret = (props:{users:Array<string>})=>{
+    console.log("users")
+    return(
+        <div>{
+            props.users.map((u,i)=> <div key={i}>{u}</div>)
+        }</div>
+    )
+}
+const Users = React.memo(UsersSecret)
+export const Example1 = ()=>{
+    const [counter,setCounter] = useState(0)
+    const [users,setUsers] = useState(["Dima","Valera","Petr"])
+    const adduser = ()=>{
+        const copyUsers = [...users,"Sveta"+new Date().getTime()]
+        setUsers(copyUsers)
+    }
+    return(
+        <div>
+            <button onClick={()=>setCounter(counter+1)}>+</button>
+            <button onClick={adduser}>+</button>
+            <NewMessagesCounter counter={counter}/>
+            <Users users={users}/>
+        </div>
+
+        )
+}
